c5119610f13665fef7e61b347dd02f38728bfcee
noob-Engle <kathiiguinagyy39@gmail.com>
2021-06-28 15:07:48 +0800

    master

--- 选择结构if/main.go
+++ 选择结构if/main.go
@@ -3,8 +3,13 @@ package main
 import "fmt"
 
 func main(){
+
 	fmt.Print("请输入xxxxx:")
 	var name string //声明一个变量字符串类型
 	fmt.Scan(&name)//接收用户输入存入name变量所在的地址
-	
+	//单分支判断
+	if name == "shabi"{ //判断用户输入如果name等于shabi就执行我是shabi，如果不是shabi的话直接打印我不是shabi
+		fmt.Println("我是shabi")
+	}
+	fmt.Println("我不是shabi")
 }
